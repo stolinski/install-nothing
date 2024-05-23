8cd15c826a698ab1aad48aac7e5c5b9faa2f0d95
Scott Tolinski <scott.tolinski@gmail.com>
2024-05-23 16:20:23 -0600

    updates readme

--- src/slides/Title.svelte
+++ src/slides/Title.svelte
@@ -1,10 +1,19 @@
 <section>
-  <h1>Fresh Style</h1>
-  <h3>The New 🔥 CSS</h3>
+  <h1>Install Nothing</h1>
+  <h3>UIs With Native Browser APIs</h3>
 </section>
 
 <style>
   h1 {
     font-size: 100px !important;
+    font-weight: 900 !important;
+    background: var(--yellow);
+    color: #000;
+    rotate: -2deg;
+    scale: 1.2;
+    margin-bottom: 5rem !important;
+  }
+
+  h3 {
   }
 </style>
